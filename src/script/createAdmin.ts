d8d4adcb904ef4da46d16465ca8af4bfdaa02126
Md Arshad Ali <arshali471@gmail.com>
2025-03-23 01:30:04 +0530

    created createAdmin.ts file

---
+++ src/script/createAdmin.ts
@@ -0,0 +1,28 @@
+import { DB } from "../config/DB";
+import userModel from "../models/user.model";
+import { Utility } from "../util/util";
+
+
+async function main() {
+   await DB.connect();
+   let adminEmail = `admin_${Math.floor(1000 + Math.random() * 9000)}@iff.com`;
+   let plainPassword = generatePassword();
+   let password = Utility.createPasswordHash(plainPassword);
+   let newAdmin = await userModel.create({ email: adminEmail, password, isAdmin: true });
+   console.log('New Admin created');
+   console.log('Email: ', adminEmail);
+   console.log('Password: ', plainPassword);
+}
+
+
+function generatePassword() {
+   var length = 8,
+       charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+       retVal = "";
+   for (var i = 0, n = charset.length; i < length; ++i) {
+       retVal += charset.charAt(Math.floor(Math.random() * n));
+   }
+   return retVal;
+}
+
+main();
